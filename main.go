6ff77e7753c9119b27399980030ef88127a05d60
gosha <gosha.say@gmail.com>
2021-03-01 00:27:37 +0300

    Add sqlite connection

--- main.go
+++ main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"database/sql"
 	b64 "encoding/base64"
 	"fmt"
 	"github.com/gorilla/websocket"
+	_ "github.com/mattn/go-sqlite3"
 	"io"
 	"log"
 	"net/http"
+	"os"
 )
 
 const html = "PCFET0NUWVBFIGh0bWw+CjxodG1sIGxhbmc9InJ1Ij4KPGhlYWQ+CiAgICA8bWV0YSBjaGFyc2V0PSJVVEYtOCIgLz4KICAgIDxtZXRhIG5hbWU9InZpZXdwb3J0IiBjb250ZW50PSJ3aWR0aD1kZXZpY2Utd2lkdGgsIGluaXRpYWwtc2NhbGU9MS4wIiAvPgogICAgPG1ldGEgaHR0cC1lcXVpdj0iWC1VQS1Db21wYXRpYmxlIiBjb250ZW50PSJpZT1lZGdlIiAvPgogICAgPHRpdGxlPkdvIFdlYlNvY2tldCBUdXRvcmlhbDwvdGl0bGU+CjwvaGVhZD4KPGJvZHk+CjxoMj5IZWxsbyBXb3JsZDwvaDI+CjxwIGlkPSJvdXRwdXQiPjwvcD4KPGxhYmVsIGZvcj0iaW5wdXQiPk1lc3NhZ2U6IDwvbGFiZWw+PGlucHV0IGlkPSJpbnB1dCIgdHlwZT0idGV4dCIgLz4KPGJ1dHRvbiBvbmNsaWNrPSJzZW5kKCkiPlNlbmQ8L2J1dHRvbj4KPHNjcmlwdD4KICAgIGxldCBzb2NrZXQgPSBuZXcgV2ViU29ja2V0KCJ3czovLzEyNy4wLjAuMTo4MDgwL3dzIik7CiAgICBsZXQgaW5wdXQgPSBkb2N1bWVudC5nZXRFbGVtZW50QnlJZCgiaW5wdXQiKTsKCiAgICBjb25zb2xlLmxvZygiQXR0ZW1wdGluZyBDb25uZWN0aW9uLi4uIik7CgovKiAgICBzb2NrZXQub25vcGVuID0gKCkgPT4gewogICAgICAgIGNvbnNvbGUubG9nKCJTdWNjZXNzZnVsbHkgQ29ubmVjdGVkIik7CiAgICAgICAgc29ja2V0LnNlbmQoIkhpIEZyb20gdGhlIENsaWVudCEiKQogICAgfTsKCiAgICBzb2NrZXQub25jbG9zZSA9IGV2ZW50ID0+IHsKICAgICAgICBjb25zb2xlLmxvZygiU29ja2V0IENsb3NlZCBDb25uZWN0aW9uOiAiLCBldmVudCk7CiAgICAgICAgc29ja2V0LnNlbmQoIkNsaWVudCBDbG9zZWQhIikKICAgIH07Ki8KCiAgICBzb2NrZXQub25lcnJvciA9IGVycm9yID0+IHsKICAgICAgICBjb25zb2xlLmxvZygiU29ja2V0IEVycm9yOiAiLCBlcnJvcik7CiAgICB9OwoKICAgIHNvY2tldC5vbm1lc3NhZ2UgPSBmdW5jdGlvbihldnQpIHsKICAgICAgICBsZXQgb3V0ID0gZG9jdW1lbnQuZ2V0RWxlbWVudEJ5SWQoJ291dHB1dCcpOwogICAgICAgIGNvbnNvbGUubG9nKGV2dCk7CiAgICAgICAgbGV0IG1zZyA9SlNPTi5wYXJzZShldnQuZGF0YSk7CiAgICAgICAgb3V0LmlubmVySFRNTCArPSBtc2cuYm9keSArICc8YnI+JzsKICAgIH07CgogICAgZnVuY3Rpb24gc2VuZCgpIHsKICAgICAgICBzb2NrZXQuc2VuZChpbnB1dC52YWx1ZSk7CiAgICAgICAgaW5wdXQudmFsdWUgPSAiIjsKICAgIH0KCjwvc2NyaXB0Pgo8L2JvZHk+CjwvaHRtbD4="
@@ -122,8 +125,40 @@ func setupRoutes() {
 
 func main() {
 	fmt.Println("Chat App v0.02")
+	if _, err := os.Stat("./data"); os.IsNotExist(err) {
+		fmt.Print("Data dir is not exists, try to create")
+		if os.Mkdir("./data", os.ModePerm) != nil {
+			log.Fatal("Can not create data dir")
+		}
+		if _, err := os.Stat("./data"); os.IsNotExist(err) {
+			log.Fatal("Dir create - fail")
+		}
+		fmt.Println(" - OK")
+	}
+	if _, err := os.Stat("./data/db.sqlite"); os.IsNotExist(err) {
+		fmt.Print("DB file is not exists, try to create")
+		_, err := os.Create("./data/db.sqlite")
+		if err != nil {
+			log.Fatal("Can not create DB file")
+		}
+		if _, err := os.Stat("./data/db.sqlite"); os.IsNotExist(err) {
+			log.Fatal("File create - fail")
+		}
+		fmt.Println(" - OK")
+	}
+	db, err := sql.Open("sqlite3", "./data/db.sqlite")
+	checkErr(err)
+
+	result, err := db.Exec("CREATE TABLE IF NOT EXISTS main.products(id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE, model  TEXT, company TEXT,vprice INTEGER)", "Apple", 72000)
+	checkErr(err)
+
+	fmt.Println(result.LastInsertId())
+	fmt.Println(result.RowsAffected())
+
+	checkErr(db.Close())
+
 	setupRoutes()
-	http.ListenAndServe(":8080", nil)
+	checkErr(http.ListenAndServe(":8080", nil))
 }
 
 func (pool *Pool) Start() {
@@ -135,14 +170,14 @@ func (pool *Pool) Start() {
 			fmt.Println("Size of Connection Pool: ", len(pool.Clients))
 			for client := range pool.Clients {
 				fmt.Println(client)
-				client.Conn.WriteJSON(Message{Type: 1, Body: "New User Joined..."})
+				checkErr(client.Conn.WriteJSON(Message{Type: 1, Body: "New User Joined..."}))
 			}
 			break
 		case client := <-pool.Unregister:
 			delete(pool.Clients, client)
 			fmt.Println("Size of Connection Pool: ", len(pool.Clients))
 			for client := range pool.Clients {
-				client.Conn.WriteJSON(Message{Type: 1, Body: "User Disconnected..."})
+				checkErr(client.Conn.WriteJSON(Message{Type: 1, Body: "User Disconnected..."}))
 			}
 			break
 		case message := <-pool.Broadcast:
@@ -160,7 +195,7 @@ func (pool *Pool) Start() {
 func (c *Client) Read() {
 	defer func() {
 		c.Pool.Unregister <- c
-		c.Conn.Close()
+		checkErr(c.Conn.Close())
 	}()
 
 	for {
@@ -183,4 +218,10 @@ func Upgrade(w http.ResponseWriter, r *http.Request) (*websocket.Conn, error) {
 	}
 
 	return conn, nil
+}
+
+func checkErr(err error) {
+	if err != nil {
+		log.Fatal(err.Error())
+	}
 }
